b7ac3251b9017a3120133dc42b29f1a5187a6b42
h2o <h2o@example.com>
2020-07-14 17:48:52 +0800

    记住我

--- src/main/java/com/example/security/configurations/SecurityConfiguration.java
+++ src/main/java/com/example/security/configurations/SecurityConfiguration.java
@@ -4,13 +4,20 @@ import com.example.security.filter.ValidateCodeFilter;
 import com.example.security.handler.AuthFailureHandler;
 import com.example.security.handler.AuthSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 /**
  * @program: security
@@ -19,7 +26,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  * @create: 2020-07-06 13:19
  **/
 @EnableWebSecurity
-public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Value("${rememberMeSeconds}")
+    private int rememberMeSeconds;
+
     @Autowired
     private ValidateCodeProperties validateCodeProperties;
 
@@ -29,6 +39,20 @@ public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthFailureHandler authFailureHandler;
 
+    @Qualifier("dataSource")
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        //tokenRepository.setCreateTableOnStartup(true);
+        return tokenRepository;
+    }
 
 
     @Bean
@@ -50,8 +74,12 @@ public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
                 .loginPage("/login.html")
                 .loginProcessingUrl("/authentication/form")
                 .and()
+                .rememberMe()
+                .tokenRepository(persistentTokenRepository())
+                .tokenValiditySeconds(rememberMeSeconds)
+                .and()
                 .authorizeRequests()
-                .antMatchers("/authentication/login","/login.html","/code/image","/authentication/form","/info2").permitAll()
+                .antMatchers("/authentication/login", "/login.html", "/code/image", "/authentication/form", "/info2").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
--- src/main/java/com/example/security/controller/UserController.java
+++ src/main/java/com/example/security/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.example.security.controller;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
